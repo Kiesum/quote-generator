4e17f2d8c01bef19438bb9ba00f9b9b986d1d2f4
Kieran <kieran.summerfeldt@gmail.com>
2017-03-24 14:56:27 -0700

    fully functional

--- main.js
+++ main.js
@@ -1,5 +1,19 @@
 $(function() {
-  console.log("hoo");
 
+  function getQuote() {
+    $.ajax({
+      url: "http://quotzzy.co/api/quote",
+      success: function(data) {
+        $('#quote').html("\"" + data.text + "\"");
+        $('#author').attr('href', data.author.wiki).html(data.author.name);
+      }
+     }); 
+  }
+
+  $('#new-quote').on('click', function() {
+    getQuote();
+  });
+
+  getQuote();
 
 });
